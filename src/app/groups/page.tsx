92b9ffd62a4c51a043c0d942c6194b44d9d85034
Akbar Bobomurodov <dev.bobomurodov@gmail.com>
2024-12-13 00:59:49 +0500

    add groups

---
+++ src/app/groups/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+
+const GroupsPage = () => {
+  console.log(process.env.NEXT_PUBLIC_SERVER_URL);
+  return <div>GroupsPage</div>;
+};
+
+export default GroupsPage;
